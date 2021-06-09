66909bca98fc1c5b01f1f5cb018b0daa91408e2c
venugopal <venugopal.reddy@onsite.co.in>
2021-06-09 09:22:03 +0530

    added communities model and removed user

--- posts/models.py
+++ posts/models.py
@@ -1,19 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from communities.models import Community
 
 # Create your models here.
 
 
 class Post(models.Model):
-    # user = models.ForeignKey(User)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     post_name = models.CharField(max_length=100)
     post_data = models.TextField()
     created_time = models.DateTimeField(auto_now_add=True)
     votes = models.IntegerField(null=True, default=0)
-    # community = models.ForeignKey(Community)
+    community = models.ForeignKey(Community, on_delete=models.CASCADE)
 
 
 class Comment(models.Model):
-    # user = models.ForeignKey(User)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     comment_data = models.CharField(max_length=255)
     created_time = models.DateTimeField(auto_now_add=True)
     votes = models.IntegerField(null=True, default=0)
