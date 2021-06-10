2db30101cff10787039910bb67d90b4c1cb6e0cb
venugopal <venugopal.reddy@onsite.co.in>
2021-06-10 10:04:59 +0530

    views and urls

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from posts.models import Post
 
 # Create your views here.
+
+
+# def index(request):
+#     return HttpResponse('This is Sample API')
+
+
+def posts_list(request):
+    posts = Post.objects.all()
+    return render(request, template_name='list_of_posts.html', context={'posts': posts}) 
